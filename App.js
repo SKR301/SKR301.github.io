3504c0fda61651c14da0578ce6e35f96fa2f501c
SKR301 <chunnu.bihat@gmail.com>
2022-08-07 21:55:59 +0530

    restarted

--- App.js
+++ App.js
@@ -1,67 +1,16 @@
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
-import colors from './assets/colors';
-import blob1 from './assets/blob1.png'; 
-import blob2 from './assets/blob2.png'; 
-import blob3 from './assets/blob3.png'; 
+import Homepage from './src/homepage';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function App() {
 	return (
-		<View style={styles.body}>
-			<View style={styles.topBar}>
-				<View><Text>Projects</Text></View>
-				<View><Text>Resume</Text></View>
-				<View><Text>Contact me</Text></View>
-			</View>
-
-			<Image source={blob2} style={[styles.blob, styles.blob2]} /> 
-			<Image source={blob3} style={[styles.blob, styles.blob3]} /> 
-			
-			<View style={styles.homeTextsContainer}>
-				<Text>&lt;Name&gt;<Text>Saurav KumaR</Text>&lt;Name&gt;</Text>
-				<Text>&lt;Profession&gt;<Text>Programming</Text>&lt;Profession&gt;</Text>
-			</View>
-		</View>
+		<Homepage />
 	);
 }
 
 
-const styles = StyleSheet.create({
-	body: {
-		flex: 1,
-		backgroundColor: colors.secondary,
-	},
-	blob: {
-		resizeMode: 'contain',
-		height: windowHeight,
-		width: windowWidth,
-		alignSelf: 'center',
-		flexDirection: 'row',
-	},
-	blob2: {
-		opacity: 0.5,
-		// paddingStart: -windowWidth*0.2
-	},
-	blob3: {
-		opacity: 0.75,
-		marginTop: -windowHeight,
-		// paddingEnd: -windowWidth*0.2
-	},
-	topBar: {
-		backgroundColor: colors.primaryDark,
-		flexDirection: 'row',
-		height: windowHeight*0.05,
-	},
-	topBarItem: {
-		flex: 1,
-		
-	},
-	topBarItemText: {
-	},
-	homeTextsContainer: {
-		marginTop: -windowHeight*0.15,
-		alignSelf: 'center'
-	}
-});
+// const styles = StyleSheet.create({
+
+// });
---
+++ src/homepage.js
@@ -0,0 +1,44 @@
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
+export default function Homepage() {
+	return (
+		<View style={homepage.body}>
+        {/* splash on bg when clicked */}
+            <View style={homepage.homepageText}>
+                <Text adjustsFontSizeToFit={true} style={homepage.salutation}>Hey, <Text style={{fontSize: 20}}>How You Doin'?</Text></Text>
+                {/* change character colour randomly on click */}
+                <Text adjustsFontSizeToFit={true} style={homepage.name}><Text>I'm </Text>Saurav KumaR</Text>
+                {/* make this program tag change automatically programmer, coder, player, developer, Puzzle lover ... */}
+                <Text adjustsFontSizeToFit={true} style={homepage.work}>Programmer</Text>
+            </View>
+		</View>
+	);
+}
+
+const homepage = StyleSheet.create({
+	body: {
+        height: windowHeight,
+		backgroundColor: '#eee'
+	},
+    homepageText: {
+        height: windowHeight,
+        marginHorizontal: 100,
+        padding: 25,
+		alignItems:'center',
+        justifyContent: 'center',
+    },
+    salutation: {
+        fontSize: 40,
+        alignSelf: 'flex-start',
+        marginStart: 50
+    },
+    name: {
+        fontSize: 100,
+    },
+    work: {
+        fontSize: 50,
+    }
+});
