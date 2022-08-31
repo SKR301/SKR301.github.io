7613657a419627f46f9b03354c15d80c35808183
SKR301 <chunnu.bihat@gmail.com>
2022-08-31 22:57:06 +0530

    added click data in state and render on each click

--- src/homepage.js
+++ src/homepage.js
@@ -1,17 +1,37 @@
 import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import { useEffect, useState } from 'react';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
 
 export default function Homepage() {
-	return (
-		<TouchableOpacity style={homepage.body} onPress={(e)=>alert(e.clientY+" "+e.clientX)}>
+    const [splash, setSplash] = useState([{}]);
+
+    const homescreenClickHandler = (e) => {
+        setSplash((splash) => [...splash, {x:e.clientX, y:e.clientY, col:'a'}]);
+    }
+
+    useEffect(() => {
+        console.log(splash);
+    }, [splash])
+
+    const splashesToRender = []
+
+    splash.map((element, index) => {
+        // show splash image here but in bg and fixed position 
+        splashesToRender.push(<Text key={index}>{element.x}</Text>)
+    });
+
+    return (
+		<View style={homepage.body} onClick={(e)=>homescreenClickHandler(e)} >
         {/* random coloured splash on bg when clicked */}
             <View>
                 {/* Top bar: Projects, Contact, Blog */}
                 {/* On top Right */}
             </View>
-
+            {
+                splashesToRender
+            }
             <View style={homepage.homepageText}>
                 <Text adjustsFontSizeToFit={true} style={homepage.salutation}>Hey, <Text style={{fontSize: 20}}>How You Doin'?</Text></Text>
                 {/* change character colour of name randomly on hover */}
@@ -19,7 +39,7 @@ export default function Homepage() {
                 {/* make this profession tag change automatically programmer, coder, player, developer, Puzzle lover ... */}
                 <Text adjustsFontSizeToFit={true} style={homepage.work}>Programmer</Text>
             </View>
-		</TouchableOpacity>
+		</View>
 	);
 }
 
