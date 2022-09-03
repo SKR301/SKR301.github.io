aab27b27cb1f492809e8d222013d29408a3feb0c
SKR301 <chunnu.bihat@gmail.com>
2022-09-03 23:30:21 +0530

    minor updates

--- src/homepage.js
+++ src/homepage.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, Dimensions, Image } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, Image, TouchableOpacity } from 'react-native';
 import { useEffect, useState, useCallback } from 'react';
 import blueSplash from '../assets/splashes/blueSplash.png'; 
 import greenSplash from '../assets/splashes/greenSplash.png'; 
@@ -10,10 +10,10 @@ import yellowSplash from '../assets/splashes/yellowSplash.png';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-// add proper professions 
+// TODO: add proper professions 
 const professionList = ['Programmer', 'Coder', 'Developer', 'Puzzle lover', 'Designer', 'Freelancer', 'Innovator'];
 let professionIndex = 0;
-// add better colors 
+// TODO: add better colors 
 const colorList = ['blue', 'red', 'green', 'springgreen', 'black', 'purple', 'yellow', 'aqua', 'coral'];
 
 export default function Homepage() {
@@ -46,28 +46,28 @@ export default function Homepage() {
     const nameToRender = [];
 
     splash.map((element, index) => {
-        // erase splash after 2 sec
-        // add random size to splashes
+        //TODO: erase splash after 2 sec
+        //TODO: add random size to splashes
         if(element.col == 1){
-            splashesToRender.push(<Image key={index} source={blueSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={blueSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 2){
-            splashesToRender.push(<Image key={index} source={greenSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={greenSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 3){
-            splashesToRender.push(<Image key={index} source={indigoSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={indigoSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 4){
-            splashesToRender.push(<Image key={index} source={orangeSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={orangeSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 5){
-            splashesToRender.push(<Image key={index} source={redSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={redSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 6){
-            splashesToRender.push(<Image key={index} source={violetSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={violetSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
         if(element.col == 7){
-            splashesToRender.push(<Image key={index} source={yellowSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-110, left: element.x-190, zIndex: -1}}></Image>)
+            splashesToRender.push(<Image key={index} source={yellowSplash} style={{ width: 200, height: 200, position: 'absolute', top: element.y-400, left: element.x-190, zIndex: -1}}></Image>)
         }
     });
 
@@ -76,10 +76,18 @@ export default function Homepage() {
     });
 
     return (
-		<View style={homepage.body} onClick={(e)=>homescreenClickHandler(e)} >
-            <View>
-                {/* Top bar: Projects, Contact, Blog */}
-                {/* On top Right */}
+		<View style={homepage.body} onClick={(e)=>homescreenClickHandler(e)}>
+            <View style={homepage.navbar}>
+                {/* add on click  */}
+                <TouchableOpacity style={homepage.navbarItem}>
+                    <Text style={homepage.navbarItemText}>Projects</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={homepage.navbarItem}>
+                    <Text style={homepage.navbarItemText}>Contact</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={homepage.navbarItem}>
+                    <Text style={homepage.navbarItemText}>Blog</Text>
+                </TouchableOpacity>
             </View>
             <View style={homepage.homepageText}>
                 {
@@ -91,7 +99,7 @@ export default function Homepage() {
                         nameToRender
                     }
                 </Text>
-                {/* make smooth transitions, fadein/slidein/random transitions etc... */}
+                {/*TODO: make smooth transitions, fadein/slidein/random transitions etc... */}
                 <View>
                     <Text adjustsFontSizeToFit={true} style={homepage.work}>{profession}</Text>
                 </View>
@@ -102,12 +110,31 @@ export default function Homepage() {
 
 const homepage = StyleSheet.create({
 	body: {
-        height: windowHeight,
 		backgroundColor: '#eee',
-	},
-    homepageText: {
         height: windowHeight,
+        flexDirection: 'column', 
+	},
+    navbar: {
+        alignSelf: 'flex-end',
+        flexDirection: 'row',
+        marginTop: 20,
+        borderLeftWidth: 20,
+        borderTopWidth: 20,
+        borderLeftColor: 'transparent',
+        borderTopColor: 'mediumblue',
+        zIndex: 1,
+    },
+    navbarItem: {
+        paddingHorizontal: 25,
+        paddingVertical: 10
+    },
+    navbarItemText:{
+        fontSize: 20
+    },
+    homepageText: {
+        // height: windowHeight,
         marginHorizontal: 100,
+        marginVertical: 200,
         padding: 25,
 		alignItems:'center',
         justifyContent: 'center',
