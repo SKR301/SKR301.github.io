b67e6213159f4dca8003c9ad586bfa0ee1fdeed5
SKR301 <chunnu.bihat@gmail.com>
2022-09-09 22:33:31 +0530

    started on project page

--- src/projects.js
+++ src/projects.js
@@ -1,4 +1,5 @@
 import { StyleSheet, Text, View, Image, Dimensions } from 'react-native';
+import Navbar from './components/navbar';
 
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
@@ -6,12 +7,29 @@ const windowHeight = Dimensions.get('window').height;
 export default function Projects() {
 	return (
 		<View>
-			<Text>Projects</Text>
+			<Navbar />
+			<View style={projects.body}>
+				<View style={projects.projectListContainer}>
+					<Text>Project List</Text>
+				</View>
+				<View style={projects.projetctSnapshot}>
+					<Text>Project Snapshot</Text>
+				</View>
+			</View>
 		</View>
 	);
 }
 
-
-const styles = StyleSheet.create({
-
+const projects = StyleSheet.create({
+	body: {
+		flexDirection: 'row',
+		flex: 2,
+		marginTop: 25,
+	},
+	projectListContainer: {
+		flex: 1
+	},
+	projetctSnapshot: {
+		flex: 1
+	},
 });
